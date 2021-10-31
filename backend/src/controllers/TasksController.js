a74e6ed57c8633430b66408be7d030674c80c89c
Matheus Antonio <matheusantonio232@gmail.com>
2021-10-31 01:48:48 -0300

    feat(taskscontroller): adicionado requisiçao para atualizar usuario

--- backend/src/controllers/TasksController.js
+++ backend/src/controllers/TasksController.js
@@ -12,4 +12,11 @@ const createNewTask = rescue(async (req, res) => {
     return res.status(201).json(data);
 });
 
-module.exports = { getAllTasks, createNewTask };
+const updateTask = rescue(async (req, res) => {
+    const { id } = req.params;
+    const { task, description, status, createdData } = req.body;
+    const data = await TasksServices.updateTask({ id, task, description, status, createdData });
+    return res.status(200).json(data);
+  });
+
+module.exports = { getAllTasks, createNewTask, updateTask };
