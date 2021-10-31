d32b2c348bf5da69179ac8954aebe714ee131871
Matheus Antonio <matheusantonio232@gmail.com>
2021-10-31 02:25:13 -0300

    feat(tasksrouter): adicionado rota para excluir tarefa

--- backend/src/routes/tasksRouter.js
+++ backend/src/routes/tasksRouter.js
@@ -4,5 +4,6 @@ const TasksController = require('../controllers/TasksController');
 router.get('/tasks/', TasksController.getAllTasks);
 router.post('/tasks/', TasksController.createNewTask);
 router.put('/tasks/:id', TasksController.updateTask);
+router.delete('/tasks/:id', TasksController.excludeTask);
 
 module.exports = router;
