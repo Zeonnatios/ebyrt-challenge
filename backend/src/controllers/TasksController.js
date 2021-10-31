18e5e0cfce1b428a3b12391d8fe54853855640d5
Matheus Antonio <matheusantonio232@gmail.com>
2021-10-31 01:27:32 -0300

    feat(taskscontroller): adicionado requisiçao para criar novo usuario (createNewTask)

--- backend/src/controllers/TasksController.js
+++ backend/src/controllers/TasksController.js
@@ -6,4 +6,10 @@ const getAllTasks = rescue(async (req, res) => {
     return res.status(200).json(data);
 });
 
-module.exports = { getAllTasks };
+const createNewTask = rescue(async (req, res) => {
+    const { task, description, status } = req.body;
+    const data = await TasksServices.createNewTask({ task, description, status });
+    return res.status(201).json(data);
+});
+
+module.exports = { getAllTasks, createNewTask };
