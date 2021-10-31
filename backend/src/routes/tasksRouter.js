ce5055cf14585cd476acfbd909ec183709a7c998
Matheus Antonio <matheusantonio232@gmail.com>
2021-10-31 01:28:04 -0300

    feat(taskrouter.js): adicionado rota de criar novo usuario

--- backend/src/routes/tasksRouter.js
+++ backend/src/routes/tasksRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const TasksController = require('../controllers/TasksController');
 
 router.get('/tasks/', TasksController.getAllTasks);
+router.post('/tasks/', TasksController.createNewTask);
 
 module.exports = router;
