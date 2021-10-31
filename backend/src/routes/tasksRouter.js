21700da53f670333960a8ae25ef61ddda165bef6
Matheus Antonio <matheusantonio232@gmail.com>
2021-10-31 01:49:39 -0300

    feat(tasksrouter.js): adicionado rota para editar tarefa

--- backend/src/routes/tasksRouter.js
+++ backend/src/routes/tasksRouter.js
@@ -3,5 +3,6 @@ const TasksController = require('../controllers/TasksController');
 
 router.get('/tasks/', TasksController.getAllTasks);
 router.post('/tasks/', TasksController.createNewTask);
+router.put('/tasks/:id', TasksController.updateTask);
 
 module.exports = router;
