b4e2c6ae9dc6522d85aef776f97f47fef6f742b0
Matheus Antonio <matheusantonio232@gmail.com>
2021-11-03 22:18:41 -0300

    fix: mudando inicializaçao da porta

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./src/api/app');
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
