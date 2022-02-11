031bec56284c2f6e84d2f5dda66539a949f86261
Matheus Antonio <matheusantonio232@gmail.com>
2022-02-11 11:25:27 -0300

    feat: add errorHandler middleware

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const { TasksRouter, UsersRouter } = require('../routes/routes');
+const { TasksRouter } = require('../routes');
+const { errorHandler } = require('../middlewares/errorHandler');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/', [TasksRouter, UsersRouter]);
+app.use('/', TasksRouter);
+app.use(errorHandler);
 
 module.exports = app;
