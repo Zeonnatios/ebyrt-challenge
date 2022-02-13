264b0c99063b80a6f8c4dc94d3267dba16166871
Matheus Antonio <matheusantonio232@gmail.com>
2022-02-12 23:59:02 -0300

    docs: swagger post

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
 const { TasksRouter } = require('../routes');
 const { errorHandler } = require('../middlewares/errorHandler');
+
+const swaggerDocs = require('./swagger.json');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/', TasksRouter);
 app.use(errorHandler);
